90232c9e4c62230c9728b1dfdbe25c7a224e36a7
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-03-13 06:35:44 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,21 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+get '/' do
+ erb :super_hero
+end
+
+post '/teams' do
+  @dt = Team.new(params[:team])
+  #@heroes = Superhero.new(params[:team])
+
+  params[:team][:members].each {|hero| Superhero.new(hero)}
+
+  @heroes = Superhero.all
+
+  erb :team
+end
+
+
 
 end
